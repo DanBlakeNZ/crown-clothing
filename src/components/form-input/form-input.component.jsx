ebed4938a274072f928f8cc2e542914d651f48e4
DanBlakeNZ <dblakenz@hotmail.com>
2020-07-27 14:17:08 +1000

    start on form input and sign in components

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./form-input.styles.scss";
+
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+  <div className="group">
+    <input className="form-input" type="text" onchange={handleChange} {...otherProps} />
+    {label ? <label className={`${otherProps.value ? "shrink" : ""} form-input-label`}>{label}</label> : null}
+  </div>
+);
+
+export default FormInput;
