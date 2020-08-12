b6c2ee72b73c161392abd19a0bb98284900f9318
DanBlakeNZ <dblakenz@hotmail.com>
2020-08-12 10:08:29 +1000

    Implemented Redux in Header and App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
 import "./App.css";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
@@ -8,17 +10,10 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     // https://firebase.google.com/docs/auth/web/manage-users#get_the_currently_signed-in_user
     // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#onauthstatechanged
     // onAuthStateChanged is an open subscription and returns firebase.Unsubscribe
@@ -28,15 +23,13 @@ class App extends React.Component {
 
         //https://firebase.google.com/docs/reference/js/firebase.firestore.DocumentSnapshot
         userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
-            },
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
           });
         });
       } else {
-        this.setState({ currentUser: userAuth }); // userAuth = null
+        setCurrentUser(userAuth); // userAuth = null
       }
     });
   }
@@ -50,7 +43,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -61,4 +54,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,4 +1,4 @@
-// Root Reducer represent all of the state of our application.
+// Root Reducer represents all of the state of our application.
 // Combines all of the other reducer states together.
 
 import { combineReducers } from "redux";
