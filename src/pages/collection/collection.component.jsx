129949d5a218571fa4e30b8c6eee41722563a760
DanBlakeNZ <dblakenz@hotmail.com>
2020-09-03 09:44:28 +1000

    moved shop data into selector. updated corisponding components

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ match }) => {
-  console.log(match);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
   return (
-    <div className="category">
-      <h2>COLLECTION PAGE</h2>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
-export default CollectionPage;
+
+// ownProps is the props of the component that we are wrapping in connect.
+// Note (state) is being passed to the selectCollection selector (because the selectCollection selector returns a function which makes use of selectShop (inside shop.selectors file) which requires the state)
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionName)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,8 +7,9 @@ import CollectionPage from "../collection/collection.component";
 const ShopPage = ({ match }) => (
   <div className="shop-page">
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
-    {/* :collectionId provides access to the collectionId within the CollectionPage component*/}
+    <Route exact path={`${match.path}/:collectionName`} component={CollectionPage} />
+
+    {/* :collectionName - this can be called whatever you want, it is simple the name we are giving to the parameter of what ever ends up after ${match.path} (much like when you add parameter names to a function). This makes it dynamic for each path. Doing this then provides the CollectionPage component with access to the 'collectionName' parameter value */}
   </div>
 );
 
